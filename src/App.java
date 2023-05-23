1f833d6f3b8565e55f5507c57b8541e3d34459fc
Andó Attila <andoattila698@gmail.com>
2023-05-23 14:34:50 +0200

    Dolgozat kezdés

---
+++ src/App.java
@@ -0,0 +1,15 @@
+/*
+* File: ButtonPanel.java
+* Author: Andó Attila
+* Copyright: 2023, Andó Attila
+* Group: Szoft I/1/N
+* Date: 2023-05-23
+* Github: https://github.com/halando/
+* Licenc: GNU GPL
+*/
+ 
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
